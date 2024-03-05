0df2134c82b3da4b5d4e103747b0ef016ffae54d
crimson-catfish <sava_kto@mail.ru>
2024-03-05 13:56:08 +0300

    simplified area expressions

--- lab3.cpp
+++ lab3.cpp
@@ -3,26 +3,30 @@
 #include <cmath>
 #include <stdio.h>
 
-
-enum Areas { M1, M2, M3, M4, M5 };
-
+enum Areas
+{
+    M1,
+    M2,
+    M3,
+    M4,
+    M5
+};
 
 class PointToCheck
 {
-    public:
-        float x, y;
+public:
+    float x, y;
 
-        bool isInSircle(int circleX, int circleY)
-        {
-            return (pow((x - circleX), 2) + pow((y - circleY), 2) <= 1);
-        }
+    bool isInSircle(int circleX, int circleY)
+    {
+        return (pow((x - circleX), 2) + pow((y - circleY), 2) <= 1);
+    }
 
-        bool isInSquare(int squareLeftX, int squareDownY, int squareRightX, int squareUpY)
-        {
-            return (x > squareLeftX && x < squareRightX && y > squareDownY && y < squareUpY);
-        }
+    bool isInSquare(int squareLeftX, int squareDownY, int squareRightX, int squareUpY)
+    {
+        return (x > squareLeftX && x < squareRightX && y > squareDownY && y < squareUpY);
+    }
 };
-
 
 int main()
 {
@@ -48,36 +52,35 @@ int main()
             area = M5;
         else
             printf("Point does not belong to any area");
-        
+
         float S;
         switch (area)
         {
-            case M1:
-                S = M_PI/4 + M_PI/12 - (M_PI/6 - sqrt(3)/4);
-                printf("Point belongs to area M1. S1 = %f", S);
-                break;
-            case M2:
-                S = M_PI/12 - (M_PI/6 - sqrt(3)/4);
-                printf("Point belongs to area M2. S2 = %f", S);
-                break;
-            case M3:
-                S = M_PI/2 + 2 - M_PI/2;
-                printf("Point belongs to area M3. S3 = %f", S);
-                break;
-            case M4:
-                S = 1 - M_PI/4 - (1 - (M_PI/4 + M_PI/12 - (M_PI/6 - sqrt(3)/4)))*2;
-                printf("Point belongs to area M4. S4 = %f", S);
-                break;
-            case M5:
-                S = (1 - (M_PI/4 + M_PI/12 - (M_PI/6 - sqrt(3)/4))) + M_PI/12 - (M_PI/6 - sqrt(3)/4) + M_PI/2;
-                printf("Point belongs to area M5. S5 = %f", S);
-                break;
+        case M1:
+            S = M_PI / 6 + sqrt(3) / 4;
+            printf("Point belongs to area M1. S1 = %f", S);
+            break;
+        case M2:
+            S = sqrt(3) / 4 - M_PI / 12;
+            printf("Point belongs to area M2. S2 = %f", S);
+            break;
+        case M3:
+            S = 2;
+            printf("Point belongs to area M3. S3 = %f", S);
+            break;
+        case M4:
+            S = M_PI / 12 - 1 + sqrt(3) / 2;
+            printf("Point belongs to area M4. S4 = %f", S);
+            break;
+        case M5:
+            S = 1 + M_PI / 4;
+            printf("Point belongs to area M5. S5 = %f", S);
+            break;
         }
 
-        
         printf("\n\nEnter \"R\" to run again, or enter any other letter to quit programm: ");
         scanf(" %c", &quitOrRestart);
     } while (quitOrRestart == 'r' || quitOrRestart == 'R');
-    
+
     return 0;
 }
