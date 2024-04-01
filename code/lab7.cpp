3c30e28636d80809edbbb2969acbee81bdffdeee
crimson-catfish <sava_kto@mail.ru>
2024-04-01 12:22:29 +0300

    lab 7 fixed

--- code/lab7.cpp
+++ code/lab7.cpp
@@ -2,50 +2,59 @@
 #include <cmath>
 using namespace std;
 
-// convex quadrilateral
-class Quad
+
+class Vertex
 {
-    private:
-        class Vertex
-        {
-            public: float x, y;
-        };
-
-        // verticies of quad
-        Vertex vrts[4];
-
-        bool are_opposite_sides_equal()
-        {
-            return (pow(vrts[0].x - vrts[1].x, 2) + pow(vrts[0].y - vrts[1].y, 2) == pow(vrts[2].x - vrts[3].x, 2) + pow(vrts[2].y - vrts[3].y, 2))
-                && (pow(vrts[1].x - vrts[2].x, 2) + pow(vrts[1].y - vrts[2].y, 2) == pow(vrts[3].x - vrts[0].x, 2) + pow(vrts[3].y - vrts[0].y, 2));
-        }
-
-    public:
-        Quad()
-        {
-            cout << "Enter X and Y for each vertex of convex quadrilateral:\n";
-            for (int i = 0; i < 4; i++)
-            {
-                cin >> vrts[i].x;
-                cin >> vrts[i].y;
-            }
-        }
-
-        bool is_parallelogram()
-        {
-            return are_opposite_sides_equal();
-        }
-
-        void is_parallelogram(bool * out)
-        {
-            *out = are_opposite_sides_equal();
-        }
-
-        void is_parallelogram(bool & out)
-        {
-            out = are_opposite_sides_equal();
-        }
+    public: float x, y;
 };
+
+float traingle_area(Vertex v1, Vertex v2, Vertex v3)
+{
+    // Calculate the sides of the triangle
+    float a = sqrt(pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2));
+    float b = sqrt(pow(v3.x - v2.x, 2) + pow(v3.y - v2.y, 2));
+    float c = sqrt(pow(v3.x - v1.x, 2) + pow(v3.y - v1.y, 2));
+
+    // Calculate the semi-perimeter of the triangle
+    double s = (a + b + c) / 2;
+
+    // Calculate the area of the triangle using Heron's formula
+    double area = sqrt(s * (s - a) * (s - b) * (s - c));
+
+    return area;
+}
+
+void traingle_area(Vertex v1, Vertex v2, Vertex v3, float * out)
+{
+    // Calculate the sides of the triangle
+    float a = sqrt(pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2));
+    float b = sqrt(pow(v3.x - v2.x, 2) + pow(v3.y - v2.y, 2));
+    float c = sqrt(pow(v3.x - v1.x, 2) + pow(v3.y - v1.y, 2));
+
+    // Calculate the semi-perimeter of the triangle
+    double s = (a + b + c) / 2;
+
+    // Calculate the area of the triangle using Heron's formula
+    double area = sqrt(s * (s - a) * (s - b) * (s - c));
+
+    *out = area;
+}
+
+void traingle_area(Vertex v1, Vertex v2, Vertex v3, float & out)
+{
+    // Calculate the sides of the triangle
+    float a = sqrt(pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2));
+    float b = sqrt(pow(v3.x - v2.x, 2) + pow(v3.y - v2.y, 2));
+    float c = sqrt(pow(v3.x - v1.x, 2) + pow(v3.y - v1.y, 2));
+
+    // Calculate the semi-perimeter of the triangle
+    double s = (a + b + c) / 2;
+
+    // Calculate the area of the triangle using Heron's formula
+    double area = sqrt(s * (s - a) * (s - b) * (s - c));
+
+    out = area;
+}
 
 int main()
 {
@@ -53,22 +62,38 @@ int main()
 
     do
     {
-        Quad quad;
+        Vertex quad[4];
+        cout << "Enter X and Y for each vertex of convex quadrilateral:\n";
+        for (int i = 0; i < 4; i++)
+        {
+            cin >> quad[i].x;
+            cin >> quad[i].y;
+        }
 
         cout << "\nOutput via return:    ";
-        if (quad.is_parallelogram()) cout << "This is a parallelogram!";
+        if (traingle_area(quad[0], quad[1], quad[2]) == traingle_area(quad[0], quad[3], quad[2])
+            && traingle_area(quad[1], quad[2], quad[3]) == traingle_area(quad[1], quad[0], quad[3]))
+        { cout << "This is a parallelogram!"; }
         else cout << "This is just a quadrilateral";
 
-        bool result;
+        float traingle_areas[4];
 
         cout << "\nOutput via pointer:   ";
-        quad.is_parallelogram(&result);
-        if (result) cout << "This is a parallelogram!";
+        traingle_area(quad[0], quad[1], quad[2], &traingle_areas[0]);
+        traingle_area(quad[0], quad[3], quad[2], &traingle_areas[1]);
+        traingle_area(quad[1], quad[2], quad[3], &traingle_areas[2]);
+        traingle_area(quad[1], quad[0], quad[3], &traingle_areas[3]);
+        if (traingle_areas[0] == traingle_areas[1] && traingle_areas[2] == traingle_areas[3])
+        { cout << "This is a parallelogram!"; }
         else cout << "This is just a quadrilateral";
 
         cout << "\nOutput via reference: ";
-        quad.is_parallelogram(result);
-        if (result) cout << "This is a parallelogram!";
+        traingle_area(quad[0], quad[1], quad[2], traingle_areas[0]);
+        traingle_area(quad[0], quad[3], quad[2], traingle_areas[1]);
+        traingle_area(quad[1], quad[2], quad[3], traingle_areas[2]);
+        traingle_area(quad[1], quad[0], quad[3], traingle_areas[3]);
+        if (traingle_areas[0] == traingle_areas[1] && traingle_areas[2] == traingle_areas[3])
+        { cout << "This is a parallelogram!"; }
         else cout << "This is just a quadrilateral";
 
         cout << "\n\nEnter \"R\" to run again, or enter any other letter to quit programm: ";
---
+++ code/lab8.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    char quitOrRestart = 'r';
+
+    do
+    {
+        int n;
+        cout << "Enter half of array length (greater than 0): ";
+        cin >> n;
+        if (n < 1)
+        {
+            cout << "length should be greater than 0";
+        }
+
+        int b[n * 2];
+        for (int i = 0; i < 2 * n; i++) { cin >> b[i]; }
+
+
+        int max_signed = -INFINITY;
+        int max_signed_index;
+        for (int i = 0; i < n * 2; i++)
+        {
+            if (b[i] < 0 && b[i] > max_signed)
+            {
+                max_signed = b[i];
+                max_signed_index = i;
+            }
+        }
+        if (max_signed == -INFINITY)
+        {
+            cout << "No signed numbers found!";
+            continue;
+        }
+
+
+        int last_before_n_unsigned_index = -1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (b[i] > 0) last_before_n_unsigned_index = i;
+        }
+        if (last_before_n_unsigned_index == -1)
+        {
+            cout << "No unsigned numbers found in first half of array!";
+            continue;
+        }
+
+        int left_bound = min(max_signed_index, last_before_n_unsigned_index);
+        int right_bound = max(max_signed_index, last_before_n_unsigned_index);
+        if (left_bound % 2 == 0) left_bound++;
+        if (right_bound < left_bound)
+        {
+            
+        }
+
+
+        cout << "\n\nEnter \"R\" to run again, or enter any other letter to quit programm: ";
+        cin >> quitOrRestart;
+    } while (quitOrRestart == 'r' || quitOrRestart == 'R');
+
+    return 0;
+}
